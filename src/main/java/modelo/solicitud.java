c239c6baf294d9656e1cba4729cbdd5b1f6fe657
emilio-baldwin12 <luisemiliobald12@gmail.com>
2026-05-05 23:44:47 -0600

    Se agrega las solicitudes para los diferentes tipos de usuario

---
+++ src/main/java/modelo/solicitud.java
@@ -0,0 +1,68 @@
+
+package modelo;
+
+/**
+ *
+ * @author luise
+ */
+import java.sql.Timestamp;
+public class solicitud {
+    private int solicitud;
+    private int idusuario;
+    private String rol;
+    private String estado;
+    private String motivo;
+    private Timestamp fecha;
+    
+    public solicitud(){
+        
+    }
+    
+    public int getsolicitud(){
+        return solicitud;
+    }
+    
+    public void setsolicitud(int solicitud){
+        this.solicitud=solicitud;
+    }
+    
+    public int getidusuario(){
+        return idusuario;
+    }
+    
+    public void setidusuario(int idusuario){
+        this.idusuario=idusuario;
+    }
+    
+    public String getrol(){
+        return rol;
+    }
+    
+    public void setrol(String rol){
+        this.rol=rol;
+    }
+    
+    public String getestado(){
+        return estado;
+    }
+    
+    public void setestado(String estado){
+        this.estado=estado;
+    }
+    
+    public String getmotivo(){
+        return motivo;
+    }
+    
+    public void setmotivo(String motivo){
+        this.motivo=motivo;
+    }
+    
+    public Timestamp getfecha(){
+        return fecha;
+    }
+    
+    public void setmotivo(Timestamp fecha){
+        this.fecha=fecha;
+    }
+}
